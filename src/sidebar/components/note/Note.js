adbf4f30cfc0c4b27d65ff6e17537f00466f10e0
cristian.cimpean <cristian.cimpean@softvision.ro>
2020-11-24 09:49:17 +0200

    Change the Note from class cu function component

--- src/sidebar/components/note/Note.js
+++ src/sidebar/components/note/Note.js
@@ -1,28 +1,22 @@
-import React from "react";
-import "./Note.css";
-import { connect } from "react-redux";
-import Box from "@material-ui/core/Box";
-import TextareaAutosize from "@material-ui/core/TextareaAutosize"
-import Button from "@material-ui/core/Button";
+import React from 'react';
+import './Note.css';
+import { connect } from 'react-redux';
+import Box from '@material-ui/core/Box';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import Button from '@material-ui/core/Button';
 import SaveOutlinedIcon from '@material-ui/icons/SaveOutlined';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import { setSidebarType, setSidebarContent } from "../../../redux/modules/sidebar/actions"
-import { NOTES } from "../../../redux/modules/sidebar/types"
-import { SAVE_TO_STORAGE } from "../../../utils/constants";
-import { sendToBackground } from "../../../utils/helpers";
-import { setNotes } from "../../../redux/modules/sidebar/actions";
+import { setSidebarType, setSidebarContent } from '../../../redux/modules/sidebar/actions';
+import { NOTES } from '../../../redux/modules/sidebar/types';
+import { SAVE_TO_STORAGE } from '../../../utils/constants';
+import { sendToBackground } from '../../../utils/helpers';
+import { setNotes } from '../../../redux/modules/sidebar/actions';
 
-class Note extends React.Component {
+const Note = ({title, content, notes, setNotes, setSidebarType, setSidebarContent}) => {
+    const handleBackButton = () => setSidebarType(NOTES);
 
-    handleBackButton = () => {
-        this.props.setSidebarType(NOTES);
-    }
-
-    handleSaveButton = () => {
-        const title = this.props.title;
-        const content = this.props.content;
-        let notes = this.props.notes ?? [];
-        let index = _.findIndex(notes, function (note) { return note.addon == title });
+    const handleSaveButton = () => {
+        let index = _.findIndex(notes ?? [], note => note.addon == title);
         if (index > -1) {
             notes[index].title = title;
             notes[index].content = content;
@@ -34,44 +28,41 @@ class Note extends React.Component {
                 date: Date.now()
             })
         }
-        sendToBackground(SAVE_TO_STORAGE, { 'notes': notes })
-        this.props.setNotes(notes);
-        this.props.setSidebarType(NOTES);
+        sendToBackground(SAVE_TO_STORAGE, { notes })
+        setNotes(notes);
+        setSidebarType(NOTES);
     }
 
-    handleContentChange = (event) => {
-        this.props.setSidebarContent(event.target.value);
+    const handleContentChange = event => {
+        setSidebarContent(event.target.value);
     }
-
-    render() {
-        return (
-            <div>
-                <Box>
-                    <h4>{this.props.title}</h4>
-                    <TextareaAutosize
-                        className='note-textarea'
-                        aria-label="minimum height"
-                        rowsMin={20}
-                        onChange={this.handleContentChange}
-                        defaultValue={this.props.content}
-                    />
-                    <Button
-                        startIcon={<ArrowBackIcon />}
-                        onClick={this.handleBackButton}
-                        style={{ margin: "0 15px" }}
-                    >Back</Button>
-                    <Button color="secondary"
-                        startIcon={<SaveOutlinedIcon />}
-                        onClick={this.handleSaveButton}
-                        className="secondary-button"
-                    >Save Note
-                    </Button>
-
-
-                </Box>
-            </div>
-        )
-    }
+   
+    return (
+        <div>
+            <Box>
+                <h4>{title}</h4>
+                <TextareaAutosize
+                    className='note-textarea'
+                    aria-label="minimum height"
+                    rowsMin={20}
+                    onChange={handleContentChange}
+                    defaultValue={content}
+                />
+                <Button
+                    startIcon={<ArrowBackIcon />}
+                    onClick={handleBackButton}
+                    style={{ margin: "0 15px" }}
+                >Back</Button>
+                <Button color="secondary"
+                    startIcon={<SaveOutlinedIcon />}
+                    onClick={handleSaveButton}
+                    className="secondary-button"
+                >Save Note
+                </Button>
+            </Box>
+        </div>
+    )
+  
 }
 
 const mapDispatchToProps = {
