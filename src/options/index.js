95f23f75a450ede59d0116661a970d00c0a384a3
iulianacse <alecse.iuliana@gmail.com>
2020-12-14 18:06:57 +0200

    Update redux on import storage

--- src/options/index.js
+++ src/options/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {Provider} from 'react-redux';
+import {createUIStore} from 'redux-webext';
 import Options from './options';
 
-ReactDOM.render(
-    <Options />,
-    document.getElementById('import-export'),
-);
+async function initApp(){
+    const store = await createUIStore();
+
+    const mountNode = document.createElement('div');
+    document.body.appendChild(mountNode);
+
+    ReactDOM.render(
+        <Provider store={store}>
+            <Options/>
+        </Provider>,
+        mountNode
+    );
+}
+
+initApp();
+
+
 
 
 
--- src/options/options.js
+++ src/options/options.js
@@ -1,17 +1,41 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import { useDispatch } from 'react-redux'
 
 import { sendToBackground } from '../utils/helpers';
 import { SAVE_TO_STORAGE } from '../utils/constants';
 
+import { SET_NOTES } from "../redux/modules/sidebar/types"
+import { SET_CATEGORIES, SET_WITH_ADDONS } from "../redux/modules/categories/types";
+
 const Options = () => {
     const [results, setResults] = useState({});
+    const dispatch = useDispatch();
     
     console.log('results', results)
 
     const handleOnClick = async () => {
         await browser.storage.local.clear();
         browser.storage.local.set(results);
+        console.log("results", results)
+        if (results.notes) {
+            dispatch({
+                type: SET_NOTES,
+                payload: results.notes
+            })    
+        }
+        if (results.categories) {
+            dispatch({
+                type: SET_CATEGORIES,
+                payload: results.categories
+            })    
+        }
+        if (results.withAddons) {
+            dispatch({
+                type: SET_WITH_ADDONS,
+                payload: results.withAddons
+            })    
+        }
         sendToBackground(SAVE_TO_STORAGE, results);
     }
 
